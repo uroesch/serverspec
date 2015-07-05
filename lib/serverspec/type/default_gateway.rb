d0d79a842b58809812cf5db2271dfdd3f1418be7
Urs Roesch <github@bun.ch>
2015-07-05 02:04:48 +0200

    Adding the distinction between ipv4 and ipv6 default_gateway  - Requires change in specinfra

--- lib/serverspec/type/default_gateway.rb
+++ lib/serverspec/type/default_gateway.rb
@@ -1,12 +1,24 @@
 module Serverspec::Type
   class DefaultGateway < Base
+    def ipv6_ipaddress
+      @runner.get_default_gateway(:ipv6_gateway)
+    end
+
     def ipaddress
       @runner.get_default_gateway(:gateway)
+    end
+
+    alias :ipv4_ipaddress :ipaddress
+
+    def ipv6_interface
+      @runner.get_default_gateway(:ipv6_interface)
     end
 
     def interface
       @runner.get_default_gateway(:interface)
     end
+
+    alias :ipv4_interface :interface
 
     def to_s
       'Default Gateway'
